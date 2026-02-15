1bc03f118e7cf61818eaff5c46d368395de93eac
Amitya31 <amitya1576@gmail.com>
2026-02-15 14:14:15 +0530

    updated again admin.tsx download functionality working

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Mail, Lock, User, Phone, Briefcase, Upload, Zap, Target } from 'lucide-react';
 import head from '../assets/logo.png';
 import landingImg from '../assets/landing.png';
@@ -50,8 +50,11 @@ export default function Landing() {
     }
   };
 
-
-  ;
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      navigate("/home");
+    }
+  }, [])
 
   const handleSignupSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
