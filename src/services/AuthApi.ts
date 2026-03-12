835debf76fa9061800201b4abe075bd63199abef
Amit Yadav <amitya1576@gmail.com>
2026-03-12 23:02:02 +0530

    updated

--- src/services/AuthApi.ts
+++ src/services/AuthApi.ts
@@ -1,5 +1,5 @@
 const login = async (email: string, password: string) => {
-    const response = await fetch("http://localhost:5000/api/auth/login", {
+    const response = await fetch("https://resumewala.co.in/api/auth/login", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
@@ -10,7 +10,7 @@ const login = async (email: string, password: string) => {
 };
 
 const register = async (email: string, fullName: string, mobileNumber: string, password: string) => {
-    const response = await fetch("http://localhost:5000/api/auth/register", {
+    const response = await fetch("https://resumewala.co.in/api/auth/register", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
