6c4c679b758010021dc0b8d89ddd6b09db785ec3
Smitayadav01 <960smitayadav@gmail.com>
2026-03-14 14:52:52 +0530

    sendemail

--- Backend/utils/sendEmail.js
+++ Backend/utils/sendEmail.js
@@ -7,25 +7,15 @@ const transporter = nodemailer.createTransport({
     pass: process.env.EMAIL_PASS,
   },
 });
--
+
 export const sendEmail = async ({ to, subject, html }) => {
   try {
-
-    // If sending to admin, split multiple emails
-   const recipients =
-  to === "ADMIN"
-    ? process.env.ADMIN_EMAIL.split(",").map(e => e.trim())
-    : to;
-
     const info = await transporter.sendMail({
       from: `"Resumewala" <${process.env.EMAIL_USER}>`,
-      to: recipients,
+      to:process.env.ADMIN_EMAIL,
       subject,
       html,
     });
-
-    console.log("Email sent:", info.response);
-
   } catch (error) {
     console.error("Email sending error:", error);
     throw error;
